a3ec8887065b2de4ae727f1e6b717d780c6ac331
Seweryn Nurczyk <snurczyk99@gmail.com>
2023-05-12 09:55:36 +0200

    Test

--- src/components/pages/SinglePost.js
+++ src/components/pages/SinglePost.js
@@ -1,7 +1,63 @@
-const SinglePost = () => {
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getPostById } from "../../redux/postsRedux";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { Modal } from "react-bootstrap";
+import { removePost } from "../../redux/postsRedux";
+
+
+const SinglePost = props => {
+    
+    const [show, setShow] = useState(false);
+
+    const handleShow = () => setShow(true)
+    const handleClose = () => setShow(false);
+
+    const dispatch = useDispatch();
+
+    const deletePost = () => {
+      dispatch(removePost(id));
+      handleClose();
+    };
+
+    const { id } = useParams();
+    const postData = useSelector(state => getPostById(state, id));
+
+    if(!postData) return <Navigate to="/" />
     return (
         <div>
-            <p>Single post</p>
+            <div className="d-flex justify-content-between">
+                <h1>{postData.title}</h1>
+                <div>
+                    <Link key={props.id} to={"/post/edit/" + id}>
+                        <Button variant="btn btn-outline-secondary">Edit</Button>
+                    </Link>
+                    <Button onClick={handleShow} variant="btn btn-outline-danger">Delete</Button>
+                </div>
+            </div>
+            <p><b>Author: </b>{postData.author} <br /><b>Published: </b>{postData.publishedDate}</p>
+            <p><b>Description: </b>{postData.shortDescription}</p>
+
+            <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Are you sure?</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <p>This operation will completely remove this post from the app. 
+              <br/>Are you sure you want to do that?
+            </p>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={handleClose} variant="secondary">Cancel</Button>
+            <Button onClick={deletePost} variant="danger">Remove</Button>
+          </Modal.Footer>
+        </Modal>
         </div>
     );
 };
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,11 +1,16 @@
 //selectors
 export const getAllPosts = state => state.posts;
+export const getPostById = ({ posts }, id) => posts.find(post => post.id === id);
+export const removePost = payload => ({ type: REMOVE_POST, payload });
 // actions
 const createActionName = actionName => `app/posts/${actionName}`;
+const REMOVE_POST = createActionName('REMOVE_POST');
 
 // action creators
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+      case REMOVE_POST:
+        return [...statePart.filter(post =>post.id !== action.payload)];
     default:
       return statePart;
   };
