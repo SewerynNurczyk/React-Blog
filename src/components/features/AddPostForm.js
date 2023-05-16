18b0adcfacd0248785ea26d10e11c2d96e95f32a
Seweryn Nurczyk <snurczyk99@gmail.com>
2023-05-16 14:13:32 +0200

    upgrade AddPostForm

--- src/components/features/AddPostForm.js
+++ src/components/features/AddPostForm.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { addPost } from "../../redux/postsRedux";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const AddPostForm = () => {
 
@@ -30,29 +31,32 @@ const AddPostForm = () => {
         <div>
             <div style={{ width: '70%' }}>
                 <Form className="" >
-                    <Form.Group className="mb-4">
+                    <Form.Group className="col-md-6">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control value={title} onChange={e => setTitle(e.target.value)} />
+                        <Form.Control placeholder='Enter title' value={title} onChange={e => setTitle(e.target.value)} />
                     </Form.Group>
-                    <Form.Group className="mb-4">
+                    <Form.Group className="col-md-6">
                         <Form.Label>Author</Form.Label>
-                        <Form.Control value={author} onChange={e => setAuthor(e.target.value)} />
+                        <Form.Control placeholder='Enter author' value={author} onChange={e => setAuthor(e.target.value)} />
                     </Form.Group>
-                    <Form.Group className="mb-4">
+                    <Form.Group className="col-md-6">
                         <Form.Label>Published date</Form.Label>
-                        <Form.Control value={publishedDate} onChange={e => setPublishedDate(e.target.value)} />
+                        <Form.Control type='date' placeholder='Enter date dd-mm-yyyy' value={publishedDate} onChange={e => setPublishedDate(e.target.value)} />
                     </Form.Group>
-                    <Form.Group className="mb-4">
+                    <Form.Group className="mb-3">
                         <Form.Label>Short description</Form.Label>
-                        <Form.Control value={shortDescription} onChange={e => setShortDescription(e.target.value)} />
+                        <Form.Control as="textarea" rows={3} placeholder='Leave a comment here' value={shortDescription} onChange={e => setShortDescription(e.target.value)} />
                     </Form.Group>
-                    <Form.Group className="mb-4">
+                    <Form.Group className="mb-10">
                         <Form.Label>Content of the post</Form.Label>
-                        <Form.Control value={content} onChange={e => setContent(e.target.value)} />
+                        <Form.Control as="textarea" rows={3} placeholder='Leave a comment here' value={content} onChange={e => setContent(e.target.value)} />
                     </Form.Group>
+                    <Link to={"/post/" + 'add'}>
+                <Button onClick={handleAdd} variant="btn btn-outline-secondary" type="submit">Add Post</Button>
+            </Link>
                 </Form>
             </div>
-            <Button onClick={handleAdd} variant="btn btn-outline-secondary" type="submit"></Button>
+           
         </div>
     );
 };
--- src/components/pages/AddPost.js
+++ src/components/pages/AddPost.js
@@ -1,11 +1,15 @@
 import AddPostForm from "../features/AddPostForm";
+//import { Button } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
 const AddPost = () => {
     return (
-        <div>
-        <p className="text-center" >AddPost</p>
-        <AddPostForm />
-        </div>
+        <Row className='justify-content-center'>
+            <div className='col-6'>
+                <h2>ADD POST</h2>
+                <AddPostForm />
+            </div>
+        </Row>
     );
 };
 
